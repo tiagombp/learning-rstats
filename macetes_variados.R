00049a80e3a381fa0caf33f0ff6dcb4ef8add827
tiagombp <tiagombp@gmail.com>
2019-01-28 17:04:23 -0200

    subindo notas do curso de ggvis

--- macetes_variados.R
+++ macetes_variados.R
@@ -2,3 +2,6 @@
 # o pulo do gato é o "origin"
 # https://stackoverflow.com/questions/43230470/how-to-convert-excel-date-format-to-proper-date-with-lubridate
 as.Date(42705, origin = "1899-12-30")
+
+# um exemplo da utilidade para se setar um aes no ggplot2
+ggplot(mtcars, aes(wt, mpg)) + geom_path(aes(color = "path")) + geom_line(aes(color = "line"))
