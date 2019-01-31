8dbb2b62f144b8bb89cdd41e95fe48260ed66c79
tiagombp <tiagombp@gmail.com>
2019-01-31 10:00:30 -0200

    fim do curso e anotações variadas.

--- macetes_variados.R
+++ macetes_variados.R
@@ -5,3 +5,14 @@ as.Date(42705, origin = "1899-12-30")
 
 # um exemplo da utilidade para se setar um aes no ggplot2
 ggplot(mtcars, aes(wt, mpg)) + geom_path(aes(color = "path")) + geom_line(aes(color = "line"))
+
+
+#rstats performance tip: if you must grow a vector in a loop, make sure there's only one reference to it so that R doesn't have to make a copy on each iteration.
+
+# 6 seconds:
+x <- integer()
+for(i in 1:5e4) x <- c(x, i)
+
+# 0.02 seconds:
+x <- integer()
+for(i in 1:5e4) x[i] <- i
